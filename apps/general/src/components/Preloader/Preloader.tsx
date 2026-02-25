6e57df7afb4dc6e0e8632de1a97bc7a36e56bcb5
evgeniusly <pak.evgeny@live.ru>
2026-02-25 09:56:13 +0300

    feat: autodeploy check

--- apps/general/src/components/Preloader/Preloader.tsx
+++ apps/general/src/components/Preloader/Preloader.tsx
@@ -14,10 +14,8 @@ export const Preloader: React.FC<IPreloaderProps> = ({ progress }) => {
   return (
     <div className={classes.preloader}>
       <img className={classes.preloaderSign} src={preloaderSign} alt="" draggable="false" />
-
       <div className={classes.title}>Вход только для взрослых</div>
       <div className={classes.text}>Уберите детей от&nbsp;экрана</div>
-
       <ProgressBar progress={progress} value={Math.round(progress * 100) + '%'} />
     </div>
   )
