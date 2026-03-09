788df52cc6374fd5108a77eb482c09f10fa13d9b
evgeniusly <pak.evgeny@live.ru>
2026-03-09 10:25:24 +0300

    feat: update client links

--- apps/general/src/data/links.ts
+++ apps/general/src/data/links.ts
@@ -1,7 +1,7 @@
 export const links = {
   aviasales: 'https://aviasales.ru/',
-  client: 'https://rich-drinks.ru/',
-  promo: 'https://rich-drinks.ru/',
+  client: 'https://eda.yandex.ru/collections/ye_ads_fmcg_multon_38064',
+  promo: 'https://eda.yandex.ru/collections/ye_ads_fmcg_multon_38064',
   rules: '/rules.pdf',
   privacy: 'https://www.aviasales.ru/privacy',
   smartCapcha: 'https://yandex.ru/legal/smartcaptcha_notice/ru/',
